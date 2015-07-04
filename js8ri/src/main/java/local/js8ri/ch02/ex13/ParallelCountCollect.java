94b9eb4d7336d889b1ae8735baf0647b0bb12a33
mikan <yutakakato@icloud.com>
2015-07-05 00:42:22 +0900

    Added ch02-ex13.

---
+++ js8ri/src/main/java/local/js8ri/ch02/ex13/ParallelCountCollect.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015. Yutaka Kato. All rights reserved.
+ * https://github.com/mikan/Java8TrainingCourse
+ */
+
+package local.js8ri.ch02.ex13;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author mikan
+ */
+public class ParallelCountCollect {
+
+    private ParallelCountCollect() {
+        // static use only
+    }
+
+    public static int[] countShortWords(Stream<String> words, int threshold) {
+        Objects.requireNonNull(words);
+        if (threshold < 0) {
+            throw new IllegalArgumentException("threshold is negative: " + threshold);
+        }
+        Map<Integer, Long> counts = words.parallel().filter(w -> w.length() < threshold)
+                .collect(Collectors.groupingBy(String::length, Collectors.counting()));
+        return IntStream.range(0, threshold).map(i -> counts.get(i) == null ? 0 : counts.get(i).intValue()).toArray();
+    }
+}
---
+++ js8ri/src/test/java/local/js8ri/ch02/ex13/ParallelCountCollectTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015. Yutaka Kato. All rights reserved.
+ * https://github.com/mikan/Java8TrainingCourse
+ */
+
+package local.js8ri.ch02.ex13;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author mikan
+ */
+public class ParallelCountCollectTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testCountShortWords_NPE() {
+        ParallelCountCollect.countShortWords(null, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCountShortWords_IAE() {
+        ParallelCountCollect.countShortWords(Stream.empty(), -1);
+    }
+
+    @Test
+    public void testCountShortWords_emptyInput() {
+        int threshold = 5;
+        int[] result = ParallelCountCollect.countShortWords(Stream.empty(), threshold);
+        assertThat(result, is(new int[]{0, 0, 0, 0, 0}));
+    }
+
+    @Test
+    public void testCountShortWords_simpleInput() {
+        int threshold = 5;
+        int[] result = ParallelCountCollect.countShortWords(Stream.iterate("A", s -> s + "A").limit(threshold + 1), threshold);
+        assertEquals(threshold, result.length);
+        assertThat(result, is(new int[]{0, 1, 1, 1, 1}));
+    }
+
+    @Test
+    public void testCountShortWords_normalInput() {
+        int threshold = 5;
+        int[] result = ParallelCountCollect.countShortWords(Stream.of("N", "C", "C", "1701", "ENTERPRISE"), threshold);
+        assertEquals(threshold, result.length);
+        assertThat(result, is(new int[]{0, 3, 0, 0, 1}));
+    }
+}
