311f08f625e0e2c47069555c7bf50732012d3854
mikan <yutakakato@icloud.com>
2015-07-05 16:17:57 +0900

    Updated ch01-ex02.

--- js8ri/src/main/java/local/js8ri/ch01/ex02/SubDirectories.java
+++ js8ri/src/main/java/local/js8ri/ch01/ex02/SubDirectories.java
@@ -6,6 +6,7 @@
 package local.js8ri.ch01.ex02;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,34 +17,86 @@ import java.util.Objects;
  */
 public class SubDirectories {
 
-    public static void main(String[] args) {
-        new SubDirectories().getSubDirectories(System.getProperty("user.home")).forEach(System.out::println);
+    /**
+     * Get a list of sub directories, lambda implementation.
+     *
+     * @param path path of parent
+     * @return list of sub directories
+     * @throws NullPointerException if path is {@code null}
+     * @throws IllegalArgumentException if path is not a directory or not exist location
+     */
+    public List<File> getSubDirectories1(String path) {
+        return searchSubDirectories1(new ArrayList<>(), pathToDirFile(path));
     }
 
-    private final List<File> list = new ArrayList<>();
+    /**
+     * Get a list of sub directories, File::isDirectory method-ref. implementation.
+     *
+     * @param path path of parent
+     * @return list of sub directories
+     * @throws NullPointerException if path is {@code null}
+     * @throws IllegalArgumentException if path is not a directory or not exist location
+     */
+    public List<File> getSubDirectories2(String path) {
+        return searchSubDirectories2(new ArrayList<>(), pathToDirFile(path));
+    }
 
-    public List<File> getSubDirectories(String path) {
+    /**
+     * Get a list of sub directories, original file filter's method-ref. implementation.
+     *
+     * @param path path of parent
+     * @return list of sub directories
+     * @throws NullPointerException if path is {@code null}
+     * @throws IllegalArgumentException if path is not a directory or not exist location
+     */
+    public List<File> getSubDirectories3(String path) {
+        return searchSubDirectories3(new ArrayList<>(), pathToDirFile(path));
+    }
+
+    private List<File> searchSubDirectories1(List<File> subtotal, File parent) {
+        Objects.requireNonNull(subtotal);
+        Objects.requireNonNull(parent);
+        List<File> subDirs = Arrays.asList(parent.listFiles(f -> f.exists() && f.isDirectory())); // lambda (with exists)
+        subtotal.addAll(subDirs);
+        subDirs.forEach(d -> searchSubDirectories1(subtotal, d));
+        return subtotal;
+    }
+
+    private List<File> searchSubDirectories2(List<File> subtotal, File parent) {
+        Objects.requireNonNull(subtotal);
+        Objects.requireNonNull(parent);
+        List<File> subDirs = Arrays.asList(parent.listFiles(File::isDirectory)); // Method ref.
+        subtotal.addAll(subDirs);
+        subDirs.forEach(d -> searchSubDirectories2(subtotal, d));
+        return subtotal;
+    }
+
+    private List<File> searchSubDirectories3(List<File> subtotal, File parent) {
+        Objects.requireNonNull(subtotal);
+        Objects.requireNonNull(parent);
+        List<File> subDirs = Arrays.asList(parent.listFiles(new MyFileFilter()::accept)); // Method ref.
+        subtotal.addAll(subDirs);
+        subDirs.forEach(d -> searchSubDirectories3(subtotal, d));
+        return subtotal;
+    }
+
+    private static class MyFileFilter implements FileFilter {
+
+        @Override
+        public boolean accept(File pathname) {
+            return pathname.isDirectory();
+        }
+    }
+
+    private static File pathToDirFile(String path) {
         Objects.requireNonNull(path);
         File dir = new File(path);
         if (!dir.isDirectory()) {
             throw new IllegalArgumentException("Not a directory: " + dir);
         }
-        list.clear();
-        addSubDirectories(dir);
-        return list;
-    }
-
-    private void addSubDirectories(File parent) {
-        File[] file = parent.listFiles(p -> {
-            return p.isDirectory();
-        });
-        if (file == null) {
-            return;
+        if (!dir.exists()) {
+            throw new IllegalArgumentException("Not exist location: " + dir);
         }
-        List<File> subDirs = Arrays.asList(file);
-        if (!subDirs.isEmpty()) {
-            list.addAll(subDirs);
-            subDirs.forEach(d -> addSubDirectories(d));
-        }
+        return dir;
     }
 }
---
+++ js8ri/src/test/java/local/js8ri/ch01/ex02/SubDirectoriesTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015. Yutaka Kato. All rights reserved.
+ * https://github.com/mikan/Java8TrainingCourse
+ */
+
+package local.js8ri.ch01.ex02;
+
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author mikan
+ */
+@RunWith(Theories.class)
+public class SubDirectoriesTest {
+
+    @DataPoint
+    public static String input = "./";
+
+    @Test(expected = NullPointerException.class)
+    public void testGetSubDirectories1_NPE() {
+        new SubDirectories().getSubDirectories1(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetSubDirectories1_IAE() {
+        new SubDirectories().getSubDirectories1("foo");
+    }
+
+    @Theory
+    public void testGetSubDirectories1_theoryInput(String path) {
+        List<File> result = new SubDirectories().getSubDirectories1(path);
+        assertNotNull(result);
+        result.forEach(f -> assertTrue(f.isDirectory()));
+    }
+
+    @Theory
+    public void testGetSubDirectories2_theoryInput(String path) {
+        List<File> result = new SubDirectories().getSubDirectories2(path);
+        assertNotNull(result);
+        result.forEach(f -> assertTrue(f.isDirectory()));
+    }
+
+    @Theory
+    public void testGetSubDirectories3_theoryInput(String path) {
+        List<File> result = new SubDirectories().getSubDirectories3(path);
+        assertNotNull(result);
+        result.forEach(f -> assertTrue(f.isDirectory()));
+    }
+}
