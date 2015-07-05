5acbc9687b4b6eac088086afe39ecde2b1f9b1e8
mikan <yutakakato@icloud.com>
2015-07-05 18:01:24 +0900

    Fixed test code of ch02-ex01.

--- js8ri/src/test/java/local/js8ri/ch02/ex01/TestParallelFor.java
+++ js8ri/src/test/java/local/js8ri/ch02/ex01/TestParallelFor.java
@@ -4,14 +4,16 @@
  */
 package local.js8ri.ch02.ex01;
 
+import org.junit.Test;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 
 /**
  * @author mikan
@@ -22,7 +24,7 @@ public class TestParallelFor {
     @Test
     public void testCounts() throws IOException {
         ParallelFor.fetchAliceDotTxt();
-        String contents = new String(Files.readAllBytes(Paths.get("src/main/res/alice.txt")), StandardCharsets.UTF_8);
+        String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/alice.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
         ParallelFor pf1 = new ParallelFor(1, 1);
         assertEquals(pf1.countSequential(words), pf1.countParallel(words));
