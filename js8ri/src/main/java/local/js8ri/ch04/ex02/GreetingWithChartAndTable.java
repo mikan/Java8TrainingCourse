e77f7b6c17a45a013770abfd91122b1707cb0a82
mikan <yutakakato@icloud.com>
2015-08-17 23:50:10 +0900

    Updated ch04-ex02.

--- js8ri/src/main/java/local/js8ri/ch04/ex02/GreetingWithChartAndTable.java
+++ js8ri/src/main/java/local/js8ri/ch04/ex02/GreetingWithChartAndTable.java
@@ -23,35 +23,42 @@ import java.util.Comparator;
 public class GreetingWithChartAndTable<S> {
 
     // From Greetings
-    private StringProperty text;
+    private volatile StringProperty text = null;
 
     // From javafx.scene.chart.Chart
-    private BooleanProperty animated;
-    private ObjectProperty<Node> legend;
-    private ObjectProperty<Node> legendSide;
-    private BooleanProperty legendVisible;
-    private StringProperty title;
-    private ObjectProperty<Side> titleSide;
+    private volatile BooleanProperty animated = null;
+    private volatile ObjectProperty<Node> legend = null;
+    private volatile ObjectProperty<Node> legendSide = null;
+    private volatile BooleanProperty legendVisible = null;
+    private volatile StringProperty title = null;
+    private volatile ObjectProperty<Side> titleSide = null;
 
     // From javafx.scene.control.TableView
-    private ObjectProperty<Callback<TableView.ResizeFeatures<S>, Boolean>> columnResizePolicy;
-    private ReadOnlyObjectProperty<Comparator<S>> comparator;
-    private BooleanProperty editable;
-    private ReadOnlyObjectProperty<TablePosition<S, ?>> editingCell;
-    private DoubleProperty fixedCellSize;
-    private ObjectProperty<TableView.TableViewFocusModel<S>> focusModel;
-    private ObjectProperty<ObservableList<S>> items;
-    private ObjectProperty<EventHandler<ScrollToEvent<TableColumn<S, ?>>>> onScrollToColumn;
-    private ObjectProperty<EventHandler<ScrollToEvent<Integer>>> onScrollTo;
-    private ObjectProperty<EventHandler<SortEvent<TableView<S>>>> onSort;
-    private ObjectProperty<Node> placeholder;
-    private ObjectProperty<Callback<TableView<S>, TableRow<S>>> rowFactory;
-    private ObjectProperty<TableView.TableViewSelectionModel<S>> selectionModel;
-    private ObjectProperty<Callback<TableView<S>, Boolean>> sortPolicy;
-    private BooleanProperty tableMenuButtonVisible;
+    private volatile ObjectProperty<Callback<TableView.ResizeFeatures<S>, Boolean>> columnResizePolicy = null;
+    private volatile ReadOnlyObjectProperty<Comparator<S>> comparator = null;
+    private volatile BooleanProperty editable = null;
+    private volatile ReadOnlyObjectProperty<TablePosition<S, ?>> editingCell = null;
+    private volatile DoubleProperty fixedCellSize = null;
+    private volatile ObjectProperty<TableView.TableViewFocusModel<S>> focusModel = null;
+    private volatile ObjectProperty<ObservableList<S>> items = null;
+    private volatile ObjectProperty<EventHandler<ScrollToEvent<TableColumn<S, ?>>>> onScrollToColumn = null;
+    private volatile ObjectProperty<EventHandler<ScrollToEvent<Integer>>> onScrollTo = null;
+    private volatile ObjectProperty<EventHandler<SortEvent<TableView<S>>>> onSort = null;
+    private volatile ObjectProperty<Node> placeholder = null;
+    private volatile ObjectProperty<Callback<TableView<S>, TableRow<S>>> rowFactory = null;
+    private volatile ObjectProperty<TableView.TableViewSelectionModel<S>> selectionModel = null;
+    private volatile ObjectProperty<Callback<TableView<S>, Boolean>> sortPolicy = null;
+    private volatile BooleanProperty tableMenuButtonVisible = null;
 
     public final StringProperty textProperty() {
-        return text == null ? new SimpleStringProperty("") : text;
+        if (text == null) {
+            synchronized(this) {
+                if (text == null) {
+                    text = new SimpleStringProperty("");
+                }
+            }
+        }
+        return text;
     }
 
     public final void setText(String newValue) {
@@ -63,86 +70,233 @@ public class GreetingWithChartAndTable<S> {
     }
 
     public final BooleanProperty animatedProperty() {
-        return animated == null ? new SimpleBooleanProperty() : animated;
+        if (animated == null) {
+            synchronized(this) {
+                if (animated == null) {
+                    animated = new SimpleBooleanProperty();
+                }
+            }
+        }
+        return animated;
     }
 
     public final ObjectProperty<Node> legendProperty() {
-        return legend == null ? new SimpleObjectProperty<>() : legend;
+        if (legend == null) {
+            synchronized(this) {
+                if (legend == null) {
+                    legend = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return legend;
     }
 
     public final ObjectProperty<Node> legendSideProperty() {
-        return legendSide == null ? new SimpleObjectProperty<>() : legendSide;
+        if (legendSide == null) {
+            synchronized(this) {
+                if (legendSide == null) {
+                    legendSide = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return legendSide;
     }
 
     public final BooleanProperty legendVisibleProperty() {
-        return legendVisible == null ? new SimpleBooleanProperty() : legendVisible;
+        if (legendVisible == null) {
+            synchronized(this) {
+                if (legendVisible == null) {
+                    legendVisible = new SimpleBooleanProperty();
+                }
+            }
+        }
+        return legendVisible;
     }
 
     public final StringProperty titleProperty() {
-        return title == null ? new SimpleStringProperty() : title;
+        if (title == null) {
+            synchronized(this) {
+                if (title == null) {
+                    title = new SimpleStringProperty();
+                }
+            }
+        }
+        return title;
     }
 
     public final ObjectProperty<Side> titleSideProperty() {
-        return titleSide == null ? new SimpleObjectProperty<>() : titleSide;
+        if (titleSide == null) {
+            synchronized(this) {
+                if (titleSide == null) {
+                    titleSide = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return titleSide;
     }
 
     public final ObjectProperty<Callback<TableView.ResizeFeatures<S>, Boolean>> columnResizePolicyProperty() {
-        return columnResizePolicy == null ? new SimpleObjectProperty<>() : columnResizePolicy;
+        if (columnResizePolicy == null) {
+            synchronized(this) {
+                if (columnResizePolicy == null) {
+                    columnResizePolicy = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return columnResizePolicy;
     }
 
     public final ReadOnlyObjectProperty<Comparator<S>> comparatorProperty() {
-        return comparator == null ? new SimpleObjectProperty<>() : comparator;
+        if (comparator == null) {
+            synchronized(this) {
+                if (comparator == null) {
+                    comparator = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return comparator;
     }
 
     public final BooleanProperty editableProperty() {
-        return editable == null ? new SimpleBooleanProperty() : editable;
+        if (editable == null) {
+            synchronized(this) {
+                if (editable == null) {
+                    editable = new SimpleBooleanProperty();
+                }
+            }
+        }
+        return editable;
     }
 
     public final ReadOnlyObjectProperty<TablePosition<S, ?>> editingCellProperty() {
-        return editingCell == null ? new SimpleObjectProperty<>() : editingCell;
+        if (editingCell == null) {
+            synchronized(this) {
+                if (editingCell == null) {
+                    editingCell = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return editingCell;
     }
 
     public final DoubleProperty fixedCellSizeProperty() {
-        return fixedCellSize == null ? new SimpleDoubleProperty() : fixedCellSize;
+        if (fixedCellSize == null) {
+            synchronized(this) {
+                if (fixedCellSize == null) {
+                    fixedCellSize = new SimpleDoubleProperty();
+                }
+            }
+        }
+        return fixedCellSize;
     }
 
     public final ObjectProperty<TableView.TableViewFocusModel<S>> focusModelProperty() {
-        return focusModel == null ? new SimpleObjectProperty<>() : focusModel;
+        if (focusModel == null) {
+            synchronized(this) {
+                if (focusModel == null) {
+                    focusModel = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return focusModel;
     }
 
     public final ObjectProperty<ObservableList<S>> itemsProperty() {
-        return items == null ? new SimpleObjectProperty<>() : items;
+        if (items == null) {
+            synchronized(this) {
+                if (items == null) {
+                    items = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return items;
     }
 
     public final ObjectProperty<EventHandler<ScrollToEvent<TableColumn<S, ?>>>> onScrollToColumnProperty() {
-        return onScrollToColumn == null ? new SimpleObjectProperty<>() : onScrollToColumn;
+        if (onScrollToColumn == null) {
+            synchronized(this) {
+                if (onScrollToColumn == null) {
+                    onScrollToColumn = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return onScrollToColumn;
     }
 
     public final ObjectProperty<EventHandler<ScrollToEvent<Integer>>> onScrollToProperty() {
-        return onScrollTo == null ? new SimpleObjectProperty<>() : onScrollTo;
+        if (onScrollTo == null) {
+            synchronized(this) {
+                if (onScrollTo == null) {
+                    onScrollTo = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return onScrollTo;
     }
 
     public final ObjectProperty<EventHandler<SortEvent<TableView<S>>>> onSortProperty() {
-        return onSort == null ? new SimpleObjectProperty<>() : onSort;
+        if (onSort == null) {
+            synchronized(this) {
+                if (onSort == null) {
+                    onSort = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return onSort;
     }
 
     public final ObjectProperty<Node> placeholderProperty() {
-        return placeholder == null ? new SimpleObjectProperty<>() : placeholder;
+        if (placeholder == null) {
+            synchronized(this) {
+                if (placeholder == null) {
+                    placeholder = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return placeholder;
     }
 
     public final ObjectProperty<Callback<TableView<S>, TableRow<S>>> rowFactoryProperty() {
-        return rowFactory == null ? new SimpleObjectProperty<>() : rowFactory;
+        if (rowFactory == null) {
+            synchronized(this) {
+                if (rowFactory == null) {
+                    rowFactory = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return rowFactory;
     }
 
     public final ObjectProperty<TableView.TableViewSelectionModel<S>> selectionModelProperty() {
-        return selectionModel == null ? new SimpleObjectProperty<>() : selectionModel;
+        if (selectionModel == null) {
+            synchronized(this) {
+                if (selectionModel == null) {
+                    selectionModel = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return selectionModel;
     }
 
     public final ObjectProperty<Callback<TableView<S>, Boolean>> sortPolicyProperty() {
-        return sortPolicy == null ? new SimpleObjectProperty<>() : sortPolicy;
+        if (sortPolicy == null) {
+            synchronized(this) {
+                if (sortPolicy == null) {
+                    sortPolicy = new SimpleObjectProperty<>();
+                }
+            }
+        }
+        return sortPolicy;
     }
 
     public final BooleanProperty tableMenuButtonVisibleProperty() {
-        return tableMenuButtonVisible == null ? new SimpleBooleanProperty() : tableMenuButtonVisible;
+        if (tableMenuButtonVisible == null) {
+            synchronized(this) {
+                if (tableMenuButtonVisible == null) {
+                    tableMenuButtonVisible = new SimpleBooleanProperty();
+                }
+            }
+        }
+        return tableMenuButtonVisible;
     }
 }
