6473b33bd1ca0ac26f900d86c5fb21ef50306666
mikan <yutakakato@icloud.com>
2015-10-09 09:50:34 +0900

    Added awaiting.

--- js8ri/src/main/java/local/js8ri/ch06/ex03/AdderBench.java
+++ js8ri/src/main/java/local/js8ri/ch06/ex03/AdderBench.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
@@ -23,8 +24,14 @@ public class AdderBench {
         AtomicLong adder = new AtomicLong();
         ExecutorService executorService = Executors.newFixedThreadPool(threads);
         Instant begin = Instant.now();
-        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit).forEach(n -> adder.incrementAndGet())));
+        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit)
+                .forEach(n -> adder.incrementAndGet())));
         executorService.shutdown();
+        try {
+            executorService.awaitTermination(3, TimeUnit.MINUTES);
+        } catch (InterruptedException ex) {
+            System.err.println("Interrupted. " + ex.getMessage());
+        }
         Instant end = Instant.now();
         System.out.println("AtomicLong: " + Duration.between(begin, end).toMillis() + "msec. Value: " + adder.longValue());
     }
@@ -33,8 +40,14 @@ public class AdderBench {
         LongAdder adder = new LongAdder();
         ExecutorService executorService = Executors.newFixedThreadPool(threads);
         Instant begin = Instant.now();
-        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit).forEach(n -> adder.increment())));
+        IntStream.range(0, threads).forEach(i -> executorService.submit(() -> IntStream.range(0, limit)
+                .forEach(n -> adder.increment())));
         executorService.shutdown();
+        try {
+            executorService.awaitTermination(3, TimeUnit.MINUTES);
+        } catch (InterruptedException ex) {
+            System.err.println("Interrupted. " + ex.getMessage());
+        }
         Instant end = Instant.now();
         System.out.println("LongAdder:  " + Duration.between(begin, end).toMillis() + "msec. Value: " + adder.longValue());
     }
